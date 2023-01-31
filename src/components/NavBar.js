cb44b11b4ede3d8503872588c2d8865a97cff18e
Nicolas Sapountzis <nsap95@gmail.com>
2023-01-30 21:14:08 -0500

    finish lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Home from "./Home.js"
+import About from "./About.js"
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    NavBar
+    <a href="#home">
+      Home
+    </a>
+    <a href="#about">
+      About
+    </a>
+  </nav>
+  )
 }
 
 export default NavBar;
